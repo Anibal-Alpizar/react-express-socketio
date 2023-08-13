885ce817a09fedba973a7a6fa07e1b07d215a0df
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-08-12 23:09:25 -0600

    sending messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,51 @@
+import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 const socket = io("/");
 
 function App() {
-  return <div>Hello World</div>;
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages([...messages, newMessage]);
+    socket.emit("message", message);
+  };
+
+  useEffect(() => {
+    socket.on("message", reciveMessage);
+    return () => {
+      socket.off("message", reciveMessage);
+    };
+  }, []);
+
+  const reciveMessage = (message) =>
+    setMessages((state) => [...state, message]);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="write a message..."
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button>Send</button>
+      </form>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>
+            {message.from} :{message.body}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ const httpServer = createServer(app);
 const io = new SocketServer(httpServer);
 
 io.on("connection", (socket) => {
-  console.log("Client connected");
+  console.log(socket.id);
+  socket.on("message", (body) => {
+    socket.broadcast.emit("message", {
+      body,
+      from: socket.id.slice(6),
+    });
+  });
 });
 
 httpServer.listen(3000, () => {

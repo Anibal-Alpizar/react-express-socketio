8ebe6a3ccd7976915eae07073e296288fabe3b40
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-08-13 16:08:11 -0600

    tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,22 +28,34 @@ function App() {
     setMessages((state) => [...state, message]);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2">Chat App</h1>
         <input
           type="text"
           placeholder="write a message..."
+          className="border-2 border-zinc-500 p-2 w-full text-black"
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button>Send</button>
+        <button className="bg-green-500 mt-2 text-white p-2 rounded-md"
+        >Send</button>
+        <ul>
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${
+                message.from === "Me" ? "bg-sky-700 " : "bg-zinc-600 ml-auto"
+              }`}
+            >
+              <span className="text-xs font-bold text-slate-400 block">
+                {message.from}
+              </span>{" "}
+              <span className="text-sm"
+              >{message.body}</span>
+            </li>
+          ))}
+        </ul>
       </form>
-      <ul>
-        {messages.map((message, index) => (
-          <li key={index}>
-            {message.from} :{message.body}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
